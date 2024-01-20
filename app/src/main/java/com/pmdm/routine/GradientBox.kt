b7857e1e13e95a10a54a1bf1ff46697b9dba6cfa
IvanBeneyto <granivan98@gmail.com>
2024-01-20 01:41:19 +0100

    Initial commit

---
+++ app/src/main/java/com/pmdm/routine/GradientBox.kt
@@ -0,0 +1,28 @@
+package com.pmdm.routine
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+val Blue = Color(0xFF09B3E4)
+val Blue2 = Color(0xFF09B3E4)
+
+@Composable
+fun GradientBox(
+    modifier: Modifier,
+    content: @Composable BoxScope.() -> Unit
+){
+    Box(
+        modifier = Modifier.background(brush = Brush.linearGradient(
+            listOf(
+                Blue, Blue2
+            )
+        ))
+    ){
+        content()
+    }
+}
---
+++ app/src/main/java/com/pmdm/routine/ui/theme/loginScreen.kt
@@ -0,0 +1,13 @@
+package com.pmdm.routine.ui.theme
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.pmdm.routine.GradientBox
+
+@Composable
+fun LoginScreen(){
+    GradientBox(modifier = Modifier.fillMaxSize()) {
+        
+    }
+}
